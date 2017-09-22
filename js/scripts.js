1a9b0f0b587c6a294e1cfa0cad997a544002ff49
Susannah Lowe <lowesusannah@gmail.com>
2017-09-22 16:52:03 -0700

    attempt to add functionality of later specifications; attempt did not succeed

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,23 @@
 var array = [ ];
 var getArray = function(input) {
   var total = [];
-    for (var index = 1; index <= 10; index++) {
-      total = (total + index).split(" ");
-      //return array;
-    }
-    return total;
 
+    for (var index = 1; index <= 10; index++) {
+      if ((input % 3 === 0)) {
+        return "ping";
+      }
+      else if ((input % 5 === 0)) {
+        return "pong";
+      }
+      else if ((input % 15 === 0))
+        return "ping-pong";
+      } else {
+        return total.append(index);
+      total = total +  " " + index + ", ";
+      var totalArray = total.split(", ", total.length - 1);
+      //return array;
+      return totalArray;
+    }
   }
     //getArray() {
   //}
@@ -22,9 +33,8 @@ var getArray = function(input) {
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
-
-    var inputValue = $("input#number");
-    var finalResult = getArray(inputValue);
+    var input = parseInt ($("input#number").val());
+    var finalResult = getArray(input);
     $("#output ul").append('<li>' + finalResult + '</li>');
     //$("#output").text(finalResult);
 
