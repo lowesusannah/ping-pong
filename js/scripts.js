ce9ba6c561bc314e9b4b12a81170ee059f6e092d
Susannah Lowe <lowesusannah@gmail.com>
2017-09-22 16:15:52 -0700

    display output in list format; bugs still not fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 //Business Logic
-var array = [];
-var getArray = function(array) {
-  var total = 0;
-    for (var index = 1; index <= 10; index ++) {
-      array += index + 1 + ","
+var array = [ ];
+var getArray = function(input) {
+  var total = [];
+    for (var index = 1; index <= 10; index++) {
+      total = (total + index).split(" ");
+      //return array;
     }
-    return (array);
+    return total;
+
   }
     //getArray() {
-      //console.log();
   //}
     //return (businessLogicArray + "," + i);
     //alert(getArray);
@@ -22,12 +23,11 @@ $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
 
-    var inputValue = parseInt($("input#number").val());
+    var inputValue = $("input#number");
     var finalResult = getArray(inputValue);
-    $("#output").text(finalResult);
+    $("#output ul").append('<li>' + finalResult + '</li>');
+    //$("#output").text(finalResult);
 
-    //$(".result-display").show();
-    //$("div.form-group").hide();
 
   });
 });
